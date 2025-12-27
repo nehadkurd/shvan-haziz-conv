acb5c11f25f865db3187ce89162ca0dfb4baa105
nehadkurd <capa05969@gmail.com>
2025-12-28 01:23:38 +0300

    Repo rewrite: add ShvanHazizCONV/UI/ExportSheet.swift

---
+++ ShvanHazizCONV/UI/ExportSheet.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import UIKit
+
+struct ExportSheet: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        UIActivityViewController(activityItems: [url], applicationActivities: nil)
+    }
+
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+}
