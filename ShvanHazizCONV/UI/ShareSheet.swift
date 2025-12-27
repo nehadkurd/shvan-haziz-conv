501cf16f7cbc6e4c5a734fb58ff9ee73dfee5c0b
nehadkurd <capa05969@gmail.com>
2025-12-28 01:40:28 +0300

    Add intelligent converter features: ShvanHazizCONV/UI/ShareSheet.swift

---
+++ ShvanHazizCONV/UI/ShareSheet.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+import UIKit
+
+struct ShareSheet: UIViewControllerRepresentable {
+    let url: URL
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        UIActivityViewController(activityItems: [url], applicationActivities: nil)
+    }
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+}
