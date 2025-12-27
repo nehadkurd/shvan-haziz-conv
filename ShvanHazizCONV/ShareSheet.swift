0404fec15364bb9466f837caba69b28feb2040ea
nehadkurd <capa05969@gmail.com>
2025-12-27 03:09:43 +0300

    Add share sheet (stable)

--- ShvanHazizCONV/ShareSheet.swift
+++ ShvanHazizCONV/ShareSheet.swift
@@ -2,8 +2,10 @@ import SwiftUI
 
 struct ShareSheet: UIViewControllerRepresentable {
     let items: [Any]
+
     func makeUIViewController(context: Context) -> UIActivityViewController {
         UIActivityViewController(activityItems: items, applicationActivities: nil)
     }
-    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+
+    func updateUIViewController(_ vc: UIActivityViewController, context: Context) {}
 }
