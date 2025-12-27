5f503ac11dc5366abe26e4ca86b3d7b828aaf1b3
nehadkurd <capa05969@gmail.com>
2025-12-27 03:09:47 +0300

    Update Models for hybrid support

--- ShvanHazizCONV/Models.swift
+++ ShvanHazizCONV/Models.swift
@@ -1,7 +1,7 @@
 import UniformTypeIdentifiers
 
+/// Simple helper model used by future phases (history/batch/pro).
 struct ConversionModel {
-
     static func supportsOffline(input: UTType, output: UTType) -> Bool {
         LocalConverter.canHandle(input: input, output: output)
     }
